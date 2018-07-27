880ad93f7b03f198147597a814a23b432f9a2829
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-27 14:27:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+//
+function scuberGreetingForFeet(feet) {
+  if (feet < 400) {
+    return "This one is on me!"
+  }
+  if (feet > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (feet > 2500) {
+    return "No can do."
+  }
+}
